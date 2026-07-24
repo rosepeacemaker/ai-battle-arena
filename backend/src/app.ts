98292014bb156212bd3ad0707fec521a1c6d2500
Rozina <roseroxina19@gmail.com>
2026-07-24 18:37:18 -0500

    Refactor app structure and components for improved organization and styling
    - Moved main App component to a new directory structure under `src/app`.
    - Created a new ChatInterface component to handle user interactions and display AI responses.
    - Introduced ArenaResponse and UserMessage components for better separation of concerns.
    - Implemented Markdown rendering for AI solutions using ReactMarkdown.
    - Added Tailwind CSS for styling and layout improvements.
    - Removed old CSS and integrated new styles for a cleaner UI.
    - Updated main entry point to reflect new component paths.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
 import express from "express"
 import runGraph from "./ai/graph.ai.service.js"
+import cors from "cors";
+
 
 const app = express()
-
+app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+}))
 
 // for health check
 app.get("/health",(req, res )  =>{
@@ -15,6 +22,20 @@ app.get("/", async (req,res)=>{
 
    const result =  await runGraph("Write a code for factorial function in js")
    res.json(result)
+})
+
+app.post("/invoke", async (req,res)=>{
+
+    const { input } = req.body
+
+    const result = await runGraph(input)
+
+    res.status(2000).json({
+        message:"Graph executed succcsessfully",
+        succsess: true,
+        data: result
+    })
+
 })
 
 
